387bec9d63a0bcc46a420587f2d5d661428910e1
Drew Noakes <git@drewnoakes.com>
2010-06-16 11:35:26 +0000

    Added license header to all source files.
    git-svn-id: https://tin-man.googlecode.com/svn/trunk@39 8bd5a46a-7d9f-eef4-cb3d-c11616db2076

--- TinMan.Tests/Geometry/PolarTest.cs
+++ TinMan.Tests/Geometry/PolarTest.cs
@@ -1,6 +1,24 @@
-﻿/*
- * Created by Drew, 07/06/2010 00:07.
+﻿#region License
+/* 
+ * This file is part of TinMan.
+ *
+ * TinMan is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TinMan is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with TinMan.  If not, see <http://www.gnu.org/licenses/>.
  */
+#endregion
+
+// Copyright Drew Noakes, http://drewnoakes.com
+// Created 07/06/2010 00:07
 
 using System;
 using NUnit.Framework;
